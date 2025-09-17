de3034c30d39be80aac602eec82e4101376db881
Medo Rei <215371699+med0rei@users.noreply.github.com>
2025-09-17 12:02:27 +0900

    feat(backend/shared/types): move RequestWithUserDto to shared types

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Post, Request, UseGuards, UsePipes } from '@nestjs/common';
 import { ZodValidationPipe } from '../shared/pipes/zod-validation.pipe';
-import type { RequestWith } from '../shared/types/request-with.d';
-import { UserDto } from '../user/schemas/user.schema';
+import type { RequestWithUserDto } from '../shared/types/request-with.d';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { loginSchema } from './schemas/auth.schema';
-
-type RequestWithUserDto = RequestWith<{ user: UserDto }>;
 
 @Controller('auth')
 export class AuthController {
--- backend/src/shared/types/request-with.d.ts
+++ backend/src/shared/types/request-with.d.ts
@@ -1,1 +1,4 @@
+import { UserDto } from '../../user/schemas/user.schema';
+
 export type RequestWith<T> = Request & T;
+export type RequestWithUserDto = RequestWith<{ user: UserDto }>;
