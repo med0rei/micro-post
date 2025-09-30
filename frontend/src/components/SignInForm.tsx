4cc14aeef78bdb6fc4b1e1cd533da7e27d824ff8
Medo Rei <215371699+med0rei@users.noreply.github.com>
2025-09-30 16:57:26 +0900

    feat(frontend/components/SignInForm): add sign-in form

--- frontend/src/components/SignInForm.tsx
+++ frontend/src/components/SignInForm.tsx
@@ -1,3 +1,31 @@
+import { Button, Input, Label, useId } from '@fluentui/react-components';
+import { LogIn } from 'lucide-react';
+
 export const SignInForm = () => {
-  return <div>Sign In Form</div>;
+  const idInputId = useId('id');
+  const passwordInputId = useId('password');
+
+  return (
+    <div>
+      <div>
+        <Label htmlFor={idInputId} size='medium'>
+          ID
+        </Label>
+        <Input id={idInputId} />
+      </div>
+
+      <div>
+        <Label htmlFor={passwordInputId} size='medium'>
+          Password
+        </Label>
+        <Input id={passwordInputId} type='password' />
+      </div>
+
+      <div>
+        <Button appearance='primary' icon={<LogIn />}>
+          Login
+        </Button>
+      </div>
+    </div>
+  );
 };
