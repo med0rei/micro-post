7cf3c66cb85d8decd513062daab577126f88d3ab
Medo Rei <215371699+med0rei@users.noreply.github.com>
2025-10-07 18:57:07 +0900

    feat(frontend/pages/Main): redirect to login page when not logged in

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,12 +1,19 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
+import { Navigate } from 'react-router';
 import { Body } from '../components/Body';
 import { Contents } from '../components/Contents';
 import { Header } from '../components/Header';
 import { Sidebar } from '../components/Sidebar';
 import { PostListContext, type PostType } from '../contexts/PostListContext';
+import { UserContext } from '../contexts/UserContext';
 
 export const Main = () => {
+  const { userInfo } = useContext(UserContext);
   const [postList, setPostList] = useState<PostType[]>([]);
+
+  if (!userInfo) {
+    return <Navigate replace to='/' />;
+  }
 
   return (
     <PostListContext value={{ postList, setPostList }}>
